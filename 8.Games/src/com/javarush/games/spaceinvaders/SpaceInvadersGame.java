6f542b56bba8ec250b17e41c91ac1d13a80e101b
AlexFil161 <115090657+AlexFil161@users.noreply.github.com>
2022-10-24 01:22:11 +0300

    step 15

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -15,7 +15,6 @@ public class SpaceInvadersGame extends Game {
 
     private List<Star> stars;
     private EnemyFleet enemyFleet;
-
     private List<Bullet> enemyBullets;
 
     @Override
@@ -27,23 +26,31 @@ public class SpaceInvadersGame extends Game {
     @Override
     public void onTurn(int step) {
         moveSpaceObjects();
+        check();
+
+        Bullet bullet = enemyFleet.fire(this);
+        if (bullet != null) {
+            enemyBullets.add(bullet);
+        }
+
         drawScene();
     }
 
     private void createGame() {
         enemyFleet = new EnemyFleet();
+        enemyBullets = new ArrayList<>();
         createStars();
-        enemyBullets = new ArrayList<Bullet>();
         drawScene();
         setTurnTimer(40);
     }
 
     private void drawScene() {
         drawField();
+        enemyFleet.draw(this);
+
         for (Bullet bullet : enemyBullets) {
             bullet.draw(this);
         }
-        enemyFleet.draw(this);
     }
 
     private void drawField() {
@@ -69,12 +76,21 @@ public class SpaceInvadersGame extends Game {
 
     private void moveSpaceObjects() {
         enemyFleet.move();
-        for (Bullet bullet : enemyBullets) {
-            bullet.move();
+
+        for (Bullet enemyBullet : enemyBullets) {
+            enemyBullet.move();
         }
     }
 
     private void removeDeadBullets() {
+        for (Bullet bullet : new ArrayList<>(enemyBullets)) {
+            if (!bullet.isAlive || bullet.y >= HEIGHT - 1) {
+                enemyBullets.remove(bullet);
+            }
+        }
+    }
 
+    private void check() {
+        removeDeadBullets();
     }
 }
