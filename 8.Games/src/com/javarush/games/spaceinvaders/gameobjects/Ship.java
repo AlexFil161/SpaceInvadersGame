ef0e10064744e1d2c4f11cf01d77daab55f6adb0
AlexFil161 <115090657+AlexFil161@users.noreply.github.com>
2022-10-24 11:47:22 +0300

    step 17

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,8 +1,8 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
 public class Ship extends GameObject {
-
     public boolean isAlive = true;
+
     public Ship(double x, double y) {
         super(x, y);
     }
